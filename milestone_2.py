5272d2e01223d42062eea69fa44d65016600f3f6
Maria-AW <maria_aw_98@hotmail.co.uk>
2023-11-16 15:22:18 +0000

    File modified since last push

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,18 @@
 import random
+
+# Task 1: Define a list of possible words 
 word_list = ["watermelon", "mango", "pineapple", "strawberry", "blueberry"]
+
+# Task 2: Choose a random word from the list 
 word = random.choice(word_list)
+
+# Task 3: Ask the user for an input
 guess = input("Enter a single letter: ")
+
+# Task 4: Check that the input is a single character
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input.")
+
+# testing push

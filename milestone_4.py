7ce4ee2a0d3281b1b34322562ccaa0a138aa614d
Maria-AW <maria_aw_98@hotmail.co.uk>
2023-11-17 19:03:41 +0000

    Complete tasks in milestone_4

--- milestone_4.py
+++ milestone_4.py
@@ -2,12 +2,12 @@ import random
 
 # Task 1: Create the class
 class Hangman:
-    def __init__(self, word_list, num_lives):
+    def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.word = random.choice(word_list)
         self.word_guessed = ["_" for x in self.word]
         self.num_letters = len(set(self.word))
-        self.num_lives = 5
+        self.num_lives = num_lives
         self.list_of_guesses = [] 
 
 # Task 2: Create methods for running the checks 
@@ -16,8 +16,14 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
           print(f"Good guess! '{guess}' is in the word.")
+          for i, letter in range(len(self.word)):
+             if letter == guess:
+                self.word_guessed[i] = guess
+          self.num_letters -= 1
         else:
-          print(f"Sorry, '{guess}' is not in the word. Try again.")
+          self.num_lives -= 1
+          print(f"Sorry, {guess} is not in the word.")
+          print(f"You have {self.num_lives} lives left")
 
     def ask_for_input(self):
         while True:
